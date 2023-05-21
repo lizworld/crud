bbec39cd7f8bbba2e9ada18172d84a038f6751a8
joonsol <lizlove0523@gmail.com>
2023-05-21 16:07:19 +0900

    read

---
+++ src/components/Read.vue
@@ -0,0 +1,26 @@
+t<template>
+  <div>
+     <h2>사용자 게시판</h2>
+      <div class="container" v-for="(value,index) in data" :key="index">
+
+            <div class="f_wrap">
+                <div class="title">{{value.title}}</div>
+                <div class="name">{{value.writer}}</div>
+              </div>
+            <div class="content">{{value.content}}</div>
+          </div>
+        <button>글쓰기</button>
+      </div>
+
+</template>
+<script>
+  import data from "@/data";
+  export default {
+    name: 'Read',
+    data() {
+      return {
+        data: data
+      };
+    }
+  };
+</script>
---
+++ src/data/index.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    index: 0,
+    title: "안녕하세요",
+    writer: "루피",
+    content: "방갑습니다. 저는 루피입니다."
+  },
+  {
+    index: 1,
+    title: "커뮤니티네요.",
+    writer: "키티",
+    content: "커뮤니티에서는 무슨 내용을"
+  },
+  {
+    index: 3,
+    title: "헬로입니다.",
+    writer: "헬로",
+    content: "사이좋게 지내보아요"
+  }
+];
